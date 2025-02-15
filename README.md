e6f2009e0c6e589db58dab51c7c27306f90e3101
saviorab1 <popww147@gmail.com>
2025-02-16 02:45:57 +0700

    Link Wallet activated

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # Directories:
-*client* is (currently) for React, assignment1
+*client* is (currently) for React + tailwindcss, assignment1
 
 *server* is for a go backend server, assignment 2. Currently extremely barebone and unusable.
-
-*static-pages* currently ignored after a bombshell was dropped
--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Features from './components/Features';
+import TradingStats from './components/TradingStats';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-mystery-dark">
+      <Navbar />
+      <Hero />
+      <div className="bg-gradient-to-b from-[#0B041A] to-mystery-dark">
+        <main className="container mx-auto px-4">
+          <Features />
+          <TradingStats />
+        </main>
+      </div>
     </div>
   );
 }
